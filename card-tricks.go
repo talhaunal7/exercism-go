fe8525ae09604c3499b358d8f526da50645565f3
talhaunal7 <talhaunal7@gmail.com>
2022-11-23 17:46:25 +0300

    first commit with 3 solutions

---
+++ card-tricks.go
@@ -0,0 +1,42 @@
+package cards
+
+// FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
+func FavoriteCards() []int {
+	return []int{2, 6, 9}
+}
+
+// GetItem retrieves an item from a slice at given position.
+// If the index is out of range, we want it to return -1.
+func GetItem(slice []int, index int) int {
+	if index <= len(slice)-1 && index >= 0 {
+		return slice[index]
+	}
+	return -1
+}
+
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range the value needs to be appended.
+func SetItem(slice []int, index, value int) []int {
+	if index <= len(slice)-1 && index >= 0 {
+		slice[index] = value
+		return slice
+	}
+	slice = append(slice, value)
+	return slice
+}
+
+// PrependItems adds an arbitrary number of values at the front of a slice.
+func PrependItems(slice []int, values ...int) []int {
+	slice = append(values, slice...)
+	return slice
+}
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+	if index <= len(slice)-1 && index >= 0 {
+		newSlice := append(slice[:index], slice[index+1:]...)
+		return newSlice
+	}
+	return slice
+
+}
